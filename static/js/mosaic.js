7f079247688e5e21c08ccd9ff526dd9c96f3873a
JasonCai0529 <caiyl0529@gmail.com>
2025-06-15 22:33:07 -0500

    css&js_directaryInit

---
+++ static/js/mosaic.js
@@ -0,0 +1,17 @@
+document.getElementById("uploadForm").addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const res = await fetch('/generate', {
+        method: "POST",
+        body: formData
+    });
+
+    const blob = await res.blob();
+    const imgURL = URL.createObjectURL(blob);
+    document.getElementById('result').innerHTML = 
+      document.getElementById('result').innerHTML = `<h3>Result:</h3><img src="${imgURL}" style="max-width:400px;">`;
+});
+
+
